488567259c0afb9ff9b2665e1dafbea34af21de5
leewin <jquery@hotmail.com>
2025-01-11 14:10:45 +0800

    add Lazy Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HOCExample from './HOCExample';
 import FormActionExample from './FormActionExample';
+// import LazyComponent from './LayzComponent';
 
+const LazyComponent = React.lazy(()=>import('./LayzComponent'))
 function App() {
   return (
     <div className="App">
       <HOCExample />
       <FormActionExample />
+      <LazyComponent/>
     </div>
   );
 }
---
+++ src/LayzComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Fragment } from "react";
+
+export default function LazyComponent(){
+  return (
+    <Fragment>
+      <p>Hello</p>
+    </Fragment>
+  )
+}
