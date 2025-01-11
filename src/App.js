3d027a483e2118317ad342c1b27eaec6b3608100
leewin <jquery@hotmail.com>
2025-01-11 14:14:47 +0800

    add Suspense and fallback

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import HOCExample from './HOCExample';
 import FormActionExample from './FormActionExample';
 // import LazyComponent from './LayzComponent';
 
-const LazyComponent = React.lazy(()=>import('./LayzComponent'))
+const LazyComponent = React.lazy(() => import('./LayzComponent'))
 function App() {
   return (
     <div className="App">
       <HOCExample />
       <FormActionExample />
-      <LazyComponent/>
+      <Suspense fallback={<div>Loading....</div>}>
+        <LazyComponent />
+      </Suspense>
     </div>
   );
 }
